aa51bbff2a3aa1e4675f0379cd7810dc9e435456
박지혜 <jhintmain@gmail.com>
2023-02-27 18:30:13 +0900

    2023-02-27 - 병령처리 추가

--- src/FindCorrectAddressService.java
+++ src/FindCorrectAddressService.java
@@ -6,125 +6,85 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FindCorrectAddressService {
-    public static final char LO_KOR = '로';
-    public static final char GIL_KOR = '길';
 
     public String setAddress(String address) {
-        // 특수문자 제거
-        //address = address.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9/s]", "");
-        address = address.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9-,() ]", "");
-        return address;
+
+        String parseAddress = "";
+
+        address = address.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9,() ]", "");    // 특수문자 제거
+        String[] arg = address.split(" ");  // 공백으로 split
+        boolean continueFlag = false;
+
+        // 1글자 문자는 붙여준다
+        for (String a : arg) {
+            if ((a.length() == 1)) {
+                parseAddress += !continueFlag ? " " + a : a;
+                continueFlag = true;
+            } else {
+                continueFlag = false;
+                parseAddress += " " + a;
+            }
+        }
+        return parseAddress;
     }
 
     public String findJuso(String address) throws IOException {
-        String findAddress = "";
+        String rn = "";
 
         // 주소 API 호출
         JusoAPIService jusoApiService = new JusoAPIService();
         String jsonResult = jusoApiService.callJusoAPI(address);
         JsonArray juso = jusoApiService.parseJsonJuso(jsonResult);
 
-        if(juso != null){
-            String rn = juso.get(0).getAsJsonObject().get("rn").getAsString();
-            findAddress = rn;
-//            System.out.println(rn +" : "+address);
-        }else{
-//            System.out.println("주소없음 : "+address);
+        if (juso != null) {
+            rn = juso.get(0).getAsJsonObject().get("rn").getAsString();
         }
-        return findAddress;
-    }
-
-    public Stack<String> addressFilter(String address) {
-
-        Stack<String> stackAddress;
-
-        stackAddress = addressFilter1(address).size() < 1 ? addressFilter2(address) : addressFilter1(address);
-
-        return stackAddress;
+        return rn;
     }
 
     // 주소 필터 1 - 정규식 이용
-    public Stack<String> addressFilter1(String address) {
-        address = setAddress(address);
+    public Stack<String> addressFilter2(String address) {
 
         Stack<String> addressList = new Stack<>();
-
         String regx = PatternDefine.PATTERN_ROAD;
+        address = setAddress(address);
 
         Matcher matcher = Pattern.compile(regx).matcher(address);
         while (matcher.find()) {
-            if(!matcher.group().equals("종로") && !matcher.group().equals("구로")){
+            // 예외처리 1 - 종로/ 구로는 통과
+            if(checkException(matcher.group())){
                 addressList.push(matcher.group());
             }
         }
 
-        if(addressList.size() <1){
-            addressList.push(address);
-        }
-
-//        System.out.println("addressFilter1");
-//        System.out.println(address);
-//        System.out.println(addressList);
-
         return addressList;
     }
 
-    // 주소 필터 2 - 커스텀
-    public Stack<String> addressFilter2(String address) {
-        address =address.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9/s]", "");
-        address =address.replaceAll("[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z]", "");
-        int uniqueWord = Math.max(address.indexOf("종로"), address.indexOf("구로"));
-        if(uniqueWord != -1){
-            address = address.substring(uniqueWord+3);
-        }
+    public Stack<String> addressFilter1(String address) {
 
         Stack<String> addressList = new Stack<>();
+        address = setAddress(address);
 
-        // 무한 루프 방지 max = 5
-        for (int cnt = 0; cnt < 5; cnt++) {
-            // 로/길 이 들어간 문자열 찾기
-            int loOrGilIndex = getLoOrGilIdx(address);
+        String regx = "[가-힣\\d]+(?:로|길)";
+        Matcher matcher = Pattern.compile(regx).matcher(address);
 
-            if (loOrGilIndex != -1) {
-                // 로/길을 찾은 index 중 큰값 기준으로 앞 부분 공백 제거
-                String trimAddr = address.substring(0, loOrGilIndex + 1);
-                address = address.substring(loOrGilIndex + 1);
-
-                // 무한 루프 방지 max = 5
-                int i = 0;
-                if (!address.equals("")) {
-
-                    for (; i < 5; i++) {
-                        // 뒤에 문자가 숫자이면 계속 더해준다
-                        char ch = address.charAt(i);
-                        if (ch >= '0' && ch <= '9') {
-                            trimAddr = trimAddr + ch;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-
-                address = address.substring(i);
-                addressList.push(trimAddr);
-            } else {
-                break;
+        while (matcher.find()) {
+            // 예외처리 1 - 종로/ 구로는 통과
+            if(checkException(matcher.group())){
+                addressList.push(matcher.group());
             }
         }
-
-        System.out.println("addressFilter2");
-        System.out.println(address);
-//        System.out.println(addressList);
-
         return addressList;
     }
 
-    /**
-     * 로 / 길이 포함되 있는 주소인지 체크
-     */
-    public int getLoOrGilIdx(String address) {
-        return Math.max(address.indexOf(LO_KOR), address.indexOf(GIL_KOR));
+    public boolean checkException(String findAddress) {
+        String[] rgExceptLo = {"종로","구로"};
+        for(String lo : rgExceptLo){
+            if(findAddress.equals(lo)){
+                return false;
+            }
+        }
+       return true;
     }
-
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,126 +1,327 @@
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.io.*;
-import java.util.Stack;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void test(String address, boolean flag) throws IOException {
+        JsonArray roadAddr = null;
 
-        FindCorrectAddressService findCorrectAddressService = new FindCorrectAddressService();
-        Stack<String> stackAddress;
+        JusoAPIService jusoAPIService = new JusoAPIService();
+
+        jusoAPIService.setCountPerPage(100);
+
+
+        // 약 3만건
+        File file_result = new File("src/data/juso_data_api.txt"); // File객체 생성
+        if (!file_result.exists()) { // 파일이 존재하지 않으면
+            file_result.createNewFile(); // 신규생성
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file_result, flag));
+
+       // writer.write("서울특별시 구로구 오류동 97-4 서울가든빌라|서울특별시 구로구 경인로 83 (오류동, 서울가든빌라)|마포구 도화-2길 코끼리분식|성남, 분당 백 현 로 265 푸른마을 아파트로 보내주세요 !!|");
+
+        for (int page = 1; page < 5; page++) {
+            jusoAPIService.setCurrentPage(page);
+
+            String jsonResult = jusoAPIService.callJusoAPI(address);
+            JsonArray juso = jusoAPIService.parseJsonJuso(jsonResult);
+
+            if(juso == null){
+                continue;
+            }
+            for (int i = 0; i < juso.size(); i++) {
+                JsonObject tt = juso.get(i).getAsJsonObject();
+                System.out.println("["+i+"]" +tt.get("roadAddr").getAsString());
+                writer.write(tt.get("roadAddr").getAsString() + "|");
+            }
+        }
+
+        writer.flush(); // 버퍼의 남은 데이터를 모두 쓰기
+        writer.close(); // 스트림 종료
+    }
+
+
+    public static Collection<List<String>> partition(Stack<String> stack) {
+        final List<String> address = new ArrayList(stack);
+        final int chunkSize = stack.size() / 20;
+        System.out.println(chunkSize);
+        final AtomicInteger counter = new AtomicInteger();
+        final Collection<List<String>> result = address.stream()
+                .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / chunkSize))
+                .values();
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(new Date());
+
+        Stack<String> stackAddress = new Stack<>();
 
         try {
-            //파일 객체 생성
-            File file = new File("src/data/juso_data.txt");
-            //입력 스트림 생성
+            // 주소 목록 읽기 약 3만건
+            File file = new File("src/data/juso_data_api.txt");
             FileReader filereader = new FileReader(file);
-            //입력 버퍼 생성
             BufferedReader bufReader = new BufferedReader(filereader);
 
             String line = "";
+
             while ((line = bufReader.readLine()) != null) {
                 String[] rgAddress = line.split("\\|");
-//                for (int i = 0; i < rgAddress.length; i++) {
-                for (int i = 0; i < 5; i++) {
-
-                    boolean findFlag = false;
+                for (int i = 0; i < rgAddress.length; i++) {
+//                for (int i = 0; i < 10000; i++) {
                     String address = rgAddress[i];
+                    stackAddress.add(address);
+                }
+            }
+            bufReader.close();
 
-                    for(int repet=0; repet<2; repet++){
-                        stackAddress = repet ==0 ? findCorrectAddressService.addressFilter1(address) : findCorrectAddressService.addressFilter2(address);
-    /*                    System.out.println(stackAddress);*/
 
-                        for (String addr : stackAddress) {
-                            String findAddr = findCorrectAddressService.findJuso(addr);
-                            if (!findAddr.equals("")) {
-//                                System.out.println(address + " : " + findAddr);
-                                findFlag = true;
-                                break;
-                            } else {
-//                                System.out.println(address + " : 올바른 주소가 아닙니다");
+            System.out.println("================HASH SET ======================");
+            Iterator<String> iterator = stackAddress.iterator();
+            System.out.println(stackAddress.size());
+            while (iterator.hasNext()) {
+                System.out.print(iterator.next() + "|");
+            }
+
+            System.out.println("================PARTITION ======================");
+
+            partition(stackAddress).parallelStream().forEach(addressList -> {
+
+                File file_result = new File("src/data/juso_data_result.txt"); // File객체 생성
+                if (!file_result.exists()) { // 파일이 존재하지 않으면
+                    try {
+                        file_result.createNewFile(); // 신규생성
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                BufferedWriter writer = null;
+                try {
+                    writer = new BufferedWriter(new FileWriter(file_result, true));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                HashSet<String> set = new HashSet<>();
+                boolean find_flag = false;
+                FindCorrectAddressService findCorrectAddressService = new FindCorrectAddressService();
+
+                for (String address : addressList) {
+
+                    Stack<String> stack_address = findCorrectAddressService.addressFilter1(address);
+
+//                    System.out.println("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : ");
+
+                    for (String addr : stack_address) {
+
+                        // HashSet에 같은 도로명 있는지 확인, 존재시 api 보내지 않는다
+                        if (set.contains(addr)) {
+                            try {
+                                writer.write("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : "+addr + "(hash)");
+                                writer.newLine();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
                             }
-                        }
-                        if(findFlag){
+                            System.out.println("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : "+addr + "(hash)");
+                            find_flag = true;
                             break;
+                        }
+
+                        String findAddr = null;
+                        try {
+                            findAddr = findCorrectAddressService.findJuso(addr);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        if (!findAddr.equals("")) {
+                            set.add(addr);
+                            try {
+                                writer.write("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : "+addr);
+                                writer.newLine();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            System.out.println("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : "+addr);
+                            find_flag = true;
+                            break;
+                        } else {
+                            System.out.print("X");
+                        }
+                    }
+
+                    if (!find_flag) {
+                        String findAddr = null;
+                        try {
+                            findAddr = findCorrectAddressService.findJuso(address);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        if (!findAddr.equals("")) {
+                            try {
+                                writer.write("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : "+findAddr);
+                                writer.newLine();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+
+                            System.out.println("["+Thread.currentThread().getName()+"]" + stack_address.toString() + address+" : "+findAddr);
+                            set.add(findAddr);
+                        } else {
+                            System.out.println("");
                         }
                     }
                 }
-            }
 
 
-            bufReader.close();
+                try {
+                    writer.flush(); // 버퍼의 남은 데이터를 모두 쓰기
+                    writer.close(); // 스트림 종료
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+
+
         } catch (FileNotFoundException e) {
-            System.out.println("???");
+            System.out.println(e);
             // TODO: handle exception
         } catch (IOException e) {
             System.out.println(e);
         }
 
+        System.out.println(new Date());
+
     }
 
-//    public static void main(String[] args) {
+
+//    public static void main(String[] args) throws IOException {
 //
-//        JusoAPIService juso = new JusoAPIService();
-//        Stack<String> addressList;
+////        test("ㅅㄱㄷ", false);
+////        test("ㅎㅇㅁ", true);
+//
+//        FindCorrectAddressService findCorrectAddressService = new FindCorrectAddressService();
+//        Stack<String> stackAddress;
+//        HashSet<String> set = new HashSet<>();
+//        boolean writer_flag = false;
 //
 //        try {
-//            //파일 객체 생성
-//            File file = new File("src/data/juso_data.txt");
-//            //입력 스트림 생성
+//            // 주소 목록 읽기
+//            File file = new File("src/data/juso_data_api.txt");
 //            FileReader filereader = new FileReader(file);
-//            //입력 버퍼 생성
 //            BufferedReader bufReader = new BufferedReader(filereader);
 //
+//            File file_result = new File("src/data/juso_data_result.txt"); // File객체 생성
+//            if (!file_result.exists()) { // 파일이 존재하지 않으면
+//                file_result.createNewFile(); // 신규생성
+//            }
+//
+//            // BufferedWriter 생성
+//            BufferedWriter writer = new BufferedWriter(new FileWriter(file_result, false));
+//
 //            String line = "";
+//
 //            while ((line = bufReader.readLine()) != null) {
 //                String[] rgAddress = line.split("\\|");
-////                for (int i = 0; i < rgAddress.length; i++) {
-//                for (int i = 0; i < 10; i++) {
-//
+//                for (int i = 0; i < rgAddress.length; i++) {
+////                for (int i = 0; i < 10; i++) {
+//                    boolean find_flag = false;
 //                    String address = rgAddress[i];
-//                    System.out.print(address + "=>");
-//                    boolean flag = true;
 //
-//                    addressList = juso.addressFilter(address);
-//                    int cnt = addressList.size();
-//                    for (int j = 0; j < cnt; j++) {
-////                        System.out.println("filter result : " + addressList.peek());
-//                        String a = juso.findJuso(addressList.pop());
-////                        System.out.println(a);
-//                        if (!a.equals("")) {
-//                            System.out.println(a);
-//                            flag = false;
+//                    stackAddress = findCorrectAddressService.addressFilter1(address);
+//
+//                    System.out.print(stackAddress);
+//                    System.out.print(" " + address + " : ");
+//
+//                    if (writer_flag) {
+//                        // 파일에 쓰기
+//                        writer.write(String.valueOf(stackAddress));
+//                        writer.write(" " + address + " : ");
+//                    }
+//
+//
+//                    for (String addr : stackAddress) {
+//
+//                        // HashSet에 같은 도로명 있는지 확인, 존재시 api 보내지 않는다
+//                        if (set.contains(addr)) {
+//                            System.out.println(addr + "(hash)");
+//                            if (writer_flag) {
+//                                writer.write(addr + "(hash)");
+//                                writer.newLine();
+//                            }
+//                            find_flag = true;
 //                            break;
+//                        }
+//
+//                        String findAddr = findCorrectAddressService.findJuso(addr);
+//                        if (!findAddr.equals("")) {
+////                            System.out.println(addr+"|"+findAddr);
+//                            set.add(addr);
+//                            System.out.println(addr);
+//                            if (writer_flag) {
+//                                writer.write(addr);
+//                                writer.newLine();
+//                            }
+//                            find_flag = true;
+//                            break;
+//                        } else {
+//                            System.out.print("X");
 //                        }
 //                    }
 //
-//                    if (flag) {
-//                        addressList = juso.addressFilter2(address);
-//                        cnt = addressList.size();
-//                        for (int j = 0; j < cnt; j++) {
-//                            String a = juso.findJuso(addressList.pop());
-//                            if (!a.equals("")) {
-//                                System.out.println(a);
-//                                break;
+//                    if (!find_flag) {
+//                        String findAddr = findCorrectAddressService.findJuso(address);
+//                        if (!findAddr.equals("")) {
+////                            System.out.println(address+"|"+findAddr);
+//                            set.add(findAddr);
+//                            System.out.println(findAddr);
+//                            if (writer_flag) {
+//                                writer.write(findAddr);
+//                                writer.newLine();
+//                            }
+//                        } else {
+//                            System.out.println("");
+//                            if (writer_flag) {
+//                                writer.newLine();
 //                            }
 //                        }
 //                    }
 //
+//                    System.out.println("================================");
+//                    if (writer_flag) {
+//                        writer.write("================================");
+//                        writer.newLine();
+//                    }
 //                }
 //            }
 //
-//            //.readLine()은 끝에 개행문자를 읽지 않는다.
+//
+//            writer.flush(); // 버퍼의 남은 데이터를 모두 쓰기
+//            writer.close(); // 스트림 종료
 //            bufReader.close();
+//
+//
+//            System.out.println("================HASH SET ======================");
+//            Iterator<String> iterator = set.iterator();
+//            while (iterator.hasNext()) {
+//                System.out.print(iterator.next() + "|");
+//            }
+//
 //        } catch (FileNotFoundException e) {
-//            System.out.println("???");
+//            System.out.println(e);
 //            // TODO: handle exception
 //        } catch (IOException e) {
 //            System.out.println(e);
 //        }
 //
-//
 //    }
-
 
 }
